99419ad77b046e79b66d293eb67413c1c948339e
RkkSt <rikako.sato@artemis-tech.jp>
2020-06-22 03:24:59 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
     <h1>タスク一覧</h1>
-    @if (count($tasks ) > 0)
+    @if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -14,6 +15,7 @@
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{!! link_to_route("tasks.show", $task->id, ["task" => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
