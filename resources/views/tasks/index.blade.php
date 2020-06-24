9c46331c60e173df740e6b29f16ba4a68a413800
RkkSt <rikako.sato@artemis-tech.jp>
2020-06-24 01:54:39 +0000

    user registration

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the tasklist</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
+@endsection
+
+@section('content')
     <h1>タスク一覧</h1>
     @if (count($tasks) > 0)
         <table class="table table-striped">
